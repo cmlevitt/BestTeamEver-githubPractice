f44c78a4d816fb518fa728718dc5915f7e5ae55a
liamasaro <lasaro@uvm.edu>
2025-04-01 17:21:24 -0400

    Fixed bugs  even_odd_site.py

--- programming files/even_odd_site.py
+++ programming files/even_odd_site.py
@@ -5,7 +5,7 @@ with open("numbers.html", "w") as f:
     f.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n")
     f.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n")
     #append numbers 1-50 into the HTML File
-    for i not in range(1, 50):
+    for i in range(1, 50):
         if i ==  2 % 0:
             g.write("<tr><td>{}</td><td></td></tr>\n".format(i))
         if not i % 3 == 0:
