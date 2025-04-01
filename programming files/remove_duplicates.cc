85a312f8e69769e89c4cc7e45eb1008b9c13f7b1
liamasaro <lasaro@uvm.edu>
2025-04-01 17:08:23 -0400

    Added bugs

--- programming files/remove_duplicates.cc
+++ programming files/remove_duplicates.cc
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+// Bugs Introduced: LA
 using namespace std;
 // function that takes in two vectors, goes through the first one, and removes duplicate values, and returns a vector of only the unique ones
 vector<int> remove_duplicates(vector<int> arr, vector<int>& removed_elements){  
     vector<int> unique_arr; // vector of unique values
-    sort(arr.begin(), arr.end());
-    int n = arr.size();
-    for(int i=0; i<n; i++){
+    sort(arr.end(), arr.begin());
+    int n = arr.size() + 1;
+    for(int i=1; i<n; i++){
         if(i == 0 || arr[i] != arr[i-1]){
             unique_arr.push_back(arr[i]);
         }
