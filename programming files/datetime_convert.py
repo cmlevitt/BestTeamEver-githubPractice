cc5ef98bcc0ed3abc327e045626b9543d4b8a83d
Lauren Knoppp <144285396+Lauren-10@users.noreply.github.com>
2025-04-01 17:23:41 -0400

    fixed bugs in datetime_convert.py

--- programming files/datetime_convert.py
+++ programming files/datetime_convert.py
@@ -1,13 +1,13 @@
-"Bugs introduced: TW"
+#Bugs introduced: TW
 from datetime import datetime
 #generate specific date and time string
 date_str = "2022-03-17 10:45:30"
 
 #parses inputted string into a datetime object
-dte_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
 
 #formats datetime object as string
-frmatted_date = date_obj.strftime('%m/%d/%Y %H:%M:%S')
+formatted_date = date_obj.strftime('%m/%d/%Y %H:%M:%S')
 
 #prints formatted date
 print(formatted_date)
