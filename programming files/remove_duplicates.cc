9430132bc85a75c9bd8c17eeb5cce0d201588fb3
Fredlorene <163361293+Fredlorene@users.noreply.github.com>
2025-04-01 17:20:16 -0400

    Update remove_duplicates.cc

--- programming files/remove_duplicates.cc
+++ programming files/remove_duplicates.cc
@@ -2,13 +2,42 @@
 #include <vector>
 #include <algorithm>
 // Bugs Introduced: LA
+/**Description A clear and concise description of what the issue/bug is.
+
+Steps to Reproduce
+
+First step to reproduce the issue/bug
+Second step to reproduce the issue/bug
+Third step to reproduce the issue/bug
+Expected Behavior A clear and concise description of what you expected to happen.
+
+Actual Behavior A clear and concise description of what actually happened.
+
+Screenshots If applicable, add screenshots to help explain the issue/bug.
+
+Additional Information
+
+Operating System:
+Browser:
+Version:
+Any other relevant information
+Possible Solution If you have any suggestions for a possible solution, please include them here.
+
+Related Issues Are there any related issues? If so, please link to them here.
+
+Priority How critical is this issue/bug? Low, medium, or high priority?
+
+Reproducibility How often can this issue/bug be reproduced? Always, sometimes, or random?
+
+Impact What is the impact of this issue/bug? Low, medium, or high?
+*/
 using namespace std;
 // function that takes in two vectors, goes through the first one, and removes duplicate values, and returns a vector of only the unique ones
 vector<int> remove_duplicates(vector<int> arr, vector<int>& removed_elements){  
     vector<int> unique_arr; // vector of unique values
-    sort(arr.end(), arr.begin());
+    sort(arr.begin(),arr.end());//
     int n = arr.size() + 1;
-    for(int i=1; i<n; i++){
+    for(int i=0; i<n; i++){
         if(i == 0 || arr[i] != arr[i-1]){
             unique_arr.push_back(arr[i]);
         }
