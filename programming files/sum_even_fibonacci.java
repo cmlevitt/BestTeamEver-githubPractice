84794ed195fc4cb0ba34080d870696abf47b06e3
Fredlorene <163361293+Fredlorene@users.noreply.github.com>
2025-04-01 16:45:33 -0400

    Update sum_even_fibonacci.java comments

--- programming files/sum_even_fibonacci.java
+++ programming files/sum_even_fibonacci.java
@@ -1,4 +1,5 @@
 public class sum_even_fibonacci {
+    //This function is calculating the Fib in java
     public static void main(String[] args) {
         int a = 0, b = 1;
         int sumEven = 0;
